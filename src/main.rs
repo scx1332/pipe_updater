98362411147d6740a1ece12ce2cbf5cffbfb3775
scx1332 <sieciech@gmail.com>
2022-09-30 15:28:22 +0200

    Added possibility of stopping multiple services and changing ownership of multiple paths

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 use lazy_static::lazy_static; // 1.4.0
 use pipe_downloader::pipe_downloader::{PipeDownloader, PipeDownloaderOptions};
+use serde_json::json;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::time::Duration;
@@ -13,38 +14,43 @@ use tokio::task;
 
 struct UpdateTask {
     downloader: Arc<Mutex<PipeDownloader>>,
-    process_to_close: Option<String>,
+    services_to_stop: Vec<String>,
     is_running: Arc<Mutex<bool>>,
+    stage: Arc<Mutex<String>>,
+    error_message: Arc<Mutex<Option<String>>>,
     paths_to_remove: Vec<PathBuf>,
     target_user: Option<String>,
     target_group: Option<String>,
-    target_path: Option<PathBuf>,
+    target_paths: Vec<PathBuf>,
 }
 
 fn update_task_main(
-    process_to_close: Option<String>,
+    services_to_stop: Vec<String>,
     downloader: Arc<Mutex<PipeDownloader>>,
     paths_to_remove: Vec<PathBuf>,
     target_user: Option<String>,
     target_group: Option<String>,
-    target_path: Option<PathBuf>,
+    target_paths: Vec<PathBuf>,
+    stage: Arc<Mutex<String>>,
 ) -> anyhow::Result<()> {
-    if let Some(process_to_close) = process_to_close.as_ref() {
-        log::info!("Stopping process: {}", process_to_close);
-        match systemctl::stop(process_to_close) {
+    *stage.lock().unwrap() = "stopping_services".to_string();
+    for service_to_stop in &services_to_stop {
+        log::info!("Stopping process: {}", service_to_stop);
+        match systemctl::stop(service_to_stop) {
             Ok(_) => {
-                log::info!("Process stopped: {}", process_to_close);
+                log::info!("Process stopped: {}", service_to_stop);
             }
             Err(e) => {
-                log::error!("Failed to stop process {}: {}", process_to_close, e);
+                log::error!("Failed to stop process {}: {}", service_to_stop, e);
                 return Err(anyhow::anyhow!(
                     "Failed to stop process {}: {}",
-                    process_to_close,
+                    service_to_stop,
                     e
                 ));
             }
         };
     }
+    *stage.lock().unwrap() = "removing_old_files".to_string();
     for path in paths_to_remove {
         if path.is_dir() {
             log::info!("Removing directory: {}", path.display());
@@ -67,6 +73,7 @@ fn update_task_main(
     thread::sleep(Duration::from_secs(1));
 
     log::info!("Start download");
+    *stage.lock().unwrap() = "downloading".to_string();
 
     match downloader.lock().unwrap().start_download() {
         Ok(_) => {}
@@ -82,35 +89,6 @@ fn update_task_main(
 
     log::info!("Download finished");
 
-    if let (Some(target_user), Some(target_group), Some(target_path)) =
-        (target_user, target_group, target_path)
-    {
-        log::info!(
-            "Changing path ownership: {} to {}:{}",
-            target_path.display(),
-            target_user,
-            target_group
-        );
-        let command = std::format!(
-            "chown -R {}:{} {}",
-            target_user,
-            target_group,
-            target_path.display()
-        )
-        .to_string();
-        match std::process::Command::new("/bin/bash")
-            .arg("-c")
-            .arg(command)
-            .output()
-        {
-            Ok(_) => {}
-            Err(e) => {
-                println!("Error changing owner: {}", e);
-                return Err(anyhow::anyhow!("Error changing owner: {}", e));
-            }
-        };
-    }
-
     if let Some(error_message) = downloader.lock().unwrap().get_progress().error_message {
         log::error!("Error downloading: {}", error_message);
         return Err(anyhow::anyhow!(
@@ -119,9 +97,42 @@ fn update_task_main(
         ));
     }
 
-    if let Some(process_to_close) = process_to_close.as_ref() {
-        log::info!("Starting process: {}", process_to_close);
-        match systemctl::restart(process_to_close) {
+    *stage.lock().unwrap() = "changing_ownership".to_string();
+
+    if let (Some(target_user), Some(target_group)) = (target_user, target_group) {
+        for target_path in target_paths {
+            log::info!(
+                "Changing path ownership: {} to {}:{}",
+                target_path.display(),
+                target_user,
+                target_group
+            );
+            let command = std::format!(
+                "chown -R {}:{} {}",
+                target_user,
+                target_group,
+                target_path.display()
+            )
+            .to_string();
+            match std::process::Command::new("/bin/bash")
+                .arg("-c")
+                .arg(command)
+                .output()
+            {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("Error changing owner: {}", e);
+                    return Err(anyhow::anyhow!("Error changing owner: {}", e));
+                }
+            };
+        }
+    }
+
+    *stage.lock().unwrap() = "starting_service".to_string();
+
+    for service_to_stop in &services_to_stop {
+        log::info!("Starting service: {}", service_to_stop);
+        match systemctl::restart(service_to_stop) {
             Ok(_) => {}
             Err(e) => {
                 log::error!("Error restarting service: {}", e);
@@ -129,26 +140,29 @@ fn update_task_main(
             }
         };
     }
+    *stage.lock().unwrap() = "finished".to_string();
     Ok(())
 }
 
 impl UpdateTask {
     fn new(
         downloader: PipeDownloader,
-        process_to_close: Option<String>,
+        services_to_stop: Vec<String>,
         paths_to_remove: Vec<PathBuf>,
         target_user: Option<String>,
         target_group: Option<String>,
-        target_path: Option<PathBuf>,
+        target_paths: Vec<PathBuf>,
     ) -> Self {
         Self {
             downloader: Arc::new(Mutex::new(downloader)),
-            process_to_close,
+            services_to_stop,
             is_running: Arc::new(Mutex::new(false)),
             paths_to_remove,
             target_user,
             target_group,
-            target_path,
+            target_paths,
+            error_message: Arc::new(Mutex::new(None)),
+            stage: Arc::new(Mutex::new("init".to_string())),
         }
     }
 
@@ -157,7 +171,11 @@ impl UpdateTask {
     }
 
     fn get_progress(self: &Self) -> serde_json::Value {
-        self.downloader.lock().unwrap().get_progress_json()
+        json!({
+            "stage": self.stage.lock().unwrap().clone(),
+            "error_message": self.error_message.lock().unwrap().clone(),
+            "downloadProgress": self.downloader.lock().unwrap().get_progress_json()
+        })
     }
 
     fn run(self: &mut Self) -> anyhow::Result<()> {
@@ -165,25 +183,29 @@ impl UpdateTask {
             return Err(anyhow::anyhow!("Task is already running"));
         }
         *self.is_running.lock().unwrap() = true;
-        let process_to_close = self.process_to_close.clone();
+        let services_to_stop = self.services_to_stop.clone();
         let downloader = self.downloader.clone();
         let is_running = self.is_running.clone();
         let paths_to_remove = self.paths_to_remove.clone();
         let target_user = self.target_user.clone();
         let target_group = self.target_group.clone();
-        let target_path = self.target_path.clone();
+        let target_paths = self.target_paths.clone();
+        let error_message = self.error_message.clone();
+        let stage = self.stage.clone();
 
         thread::spawn(move || {
             match update_task_main(
-                process_to_close,
+                services_to_stop,
                 downloader,
                 paths_to_remove,
                 target_user,
                 target_group,
-                target_path,
+                target_paths,
+                stage,
             ) {
                 Ok(_) => {}
                 Err(e) => {
+                    *error_message.lock().unwrap() = Some(e.to_string());
                     println!("Error running update task: {}", e);
                 }
             };
@@ -236,7 +258,9 @@ async fn progress_endpoint() -> impl Responder {
     {
         return web::Json(progress);
     };
-    return web::Json(serde_json::json!({}));
+    return web::Json(
+        serde_json::json!({"downloadProgress": serde_json::Value::Null, "stage": "no_task", "error_message": serde_json::Value::Null}),
+    );
 }
 
 #[get("/start")]
@@ -254,16 +278,26 @@ async fn start_update() -> impl Responder {
             let output_dir =
                 PathBuf::from(env::var("OUTPUT_DIR").unwrap_or_else(|_| "output".into()));
             let delete_dirs = env::var("DELETE_DIRS")
-                .unwrap_or_else(|_| "output".into())
-                .split(";")
-                .map(|s| PathBuf::from(s))
-                .collect::<Vec<_>>();
+                .map(|s| {
+                    s.split(";")
+                        .map(|spl| PathBuf::from(spl))
+                        .collect::<Vec<_>>()
+                })
+                .unwrap_or_else(|_| vec![]);
 
-            let process_to_close = env::var("PROCESS_CLOSE").map(|v| Some(v)).unwrap_or(None);
+            let services_to_stop = env::var("SERVICES_TO_STOP")
+                .map(|s| s.split(";").map(|spl| spl.to_string()).collect::<Vec<_>>())
+                .unwrap_or_else(|_| vec![]);
+            println!("Services to stop: {:?}", services_to_stop);
             let target_user = env::var("TARGET_USER").unwrap_or_else(|_| "erigon".into());
             let target_group = env::var("TARGET_GROUP").unwrap_or_else(|_| "erigon".into());
-            let target_change_owner_dir =
-                PathBuf::from(env::var("CHANGE_OWNER_PATH").unwrap_or_else(|_| "erigon".into()));
+            let target_change_owner_paths = env::var("CHANGE_OWNER_PATHS")
+                .map(|s| {
+                    s.split(";")
+                        .map(|spl| PathBuf::from(spl))
+                        .collect::<Vec<_>>()
+                })
+                .unwrap_or_else(|_| vec![]);
 
             let url = env::var("ARCHIVE_URL")
                 .unwrap_or_else(|_| "http://mumbai-main.golem.network:14372/beacon.tar.lz4".into());
@@ -272,11 +306,11 @@ async fn start_update() -> impl Responder {
 
             let mut updater = UpdateTask::new(
                 pd,
-                process_to_close,
+                services_to_stop,
                 delete_dirs,
                 Some(target_user),
                 Some(target_group),
-                Some(target_change_owner_dir),
+                target_change_owner_paths,
             );
             if let Err(e) = updater.run() {
                 println!("Error starting update task: {}", e);
